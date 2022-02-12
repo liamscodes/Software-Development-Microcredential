ea676b25943c2b745fd8224f9c92be13fdaa41e2
liamscodes <80784515+liamscodes@users.noreply.github.com>
2022-02-11 19:18:53 -0500

    Add files via upload

---
+++ Liam_ElkinsActivity4/top.js
@@ -0,0 +1,8 @@
+let toToop = document.querySelector('.topUp');
+
+window.addEventListener('load', function(){
+  toTop.style.display = "none";
+});
+window.addEventListener('scroll', function()){
+  if (document.body.scrollTop > 10 || )
+}
